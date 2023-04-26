b390ebff8e1c2c7f778d9902ded2579e923b882d
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2023-04-26 20:13:50 +0530

    darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*html"],
+  darkMode: 'class',
   theme: {
+    
     extend: {},
   },
   plugins: [],
